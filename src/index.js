5ff1488cd53a763b0ff2626d0d2afe3ca01e05cb
David Giraldo <davidg03072001@gmail.com>
2022-11-15 06:53:29 -0500

    updatedrouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MainRouter from './Router/MainRouter';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
+ <MainRouter>
     <App />
+  </MainRouter>
  
 );
